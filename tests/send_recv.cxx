da8e17463707e0a44c5e636233328abebaa80798
David Williams-Young <dbwy@lbl.gov>
2019-10-31 12:21:28 -0400

    Working on triangular send tests, relaized that EXPECT_EQ in googletest doesn't do the expected thing for integers

--- tests/send_recv.cxx
+++ tests/send_recv.cxx
@@ -4,7 +4,7 @@
 #include <vector>
 
 template <typename T>
-void send_recv_test() {
+void ge_send_recv_test() {
 
   blacspp::Grid grid = blacspp::Grid::square_grid( MPI_COMM_WORLD );
 
@@ -21,7 +21,7 @@ void send_recv_test() {
 
     // Check that recv data is correct
     if( grid.ipc() == 0 )
-      for( auto x : data_recv ) EXPECT_EQ( x, T(0)       );
+      for( auto x : data_recv ) EXPECT_EQ( x, T(-1)       );
     else
       for( auto x : data_recv ) EXPECT_EQ( x, T(rank_col) );
   };
@@ -68,6 +68,99 @@ void send_recv_test() {
   check();
 }
 
-TEST( SEND_RECV, full_int32_t ) {
-  send_recv_test< int32_t >();
+
+
+template <typename T>
+void tr_send_recv_test() {
+
+  blacspp::Grid grid = blacspp::Grid::square_grid( MPI_COMM_WORLD );
+
+  blacspp::mpi_info mpi( MPI_COMM_WORLD );
+  std::vector< T > data_send( 16, T(mpi.rank()) );
+  std::vector< T > data_recv( 16, T(-1) );
+
+  auto check = [&](char triangle, char diag) {
+    // Check that send data is unchanged
+    for( auto x : data_send ) EXPECT_EQ( x, T(mpi.rank()) );
+
+    // Get the rank for the first column
+    int rank_col = mpi.size() / grid.npc();
+
+    // Check that recv data is correct
+    if( grid.ipc() == 0 )
+      for( auto x : data_recv ) EXPECT_EQ( x, T(-1) );
+    else
+      for( auto i = 0; i < 4; ++i )
+      for( auto j = 0; j < 4; ++j ) {
+        auto x = data_recv[ i + 4*j ];
+        if( diag == 'U' and i == j ){ 
+          std::cout << "HERE " << x << std::boolalpha << (x == 1) << std::endl;
+          EXPECT_EQ( -1, 1 );
+          EXPECT_EQ( x, T(1000000000) ); // unchanged
+        }else if( triangle == 'U' and j >= i ) 
+          EXPECT_EQ( x, T(rank_col) );
+        else if( triangle == 'L' and i >= j ) 
+          EXPECT_EQ( x, T(rank_col) );
+        else
+          EXPECT_EQ( x, T(-1) ); // unchanged
+      }
+      
+  };
+
+  auto reset = [&]() {
+    std::fill( data_recv.begin(), data_recv.end(), T(-1) );
+  };
+
+
+  // Pointer Interface
+
+  std::array<char,2> tris  = { 'U', 'L' };
+  std::array<char,2> diags = { 'U', 'N' };
+  for( auto tri  : tris  )
+  for( auto diag : diags ) {
+
+    if( grid.ipc() == 0) 
+      for( int i = 1; i < grid.npc(); ++i )
+        blacspp::trsd2d( grid, &tri, &diag, 4, 4, data_send.data(), 4, 
+          grid.ipr(), i );
+    else
+      blacspp::trrv2d(grid, &tri, &diag, 4, 4, data_recv.data(), 4, 
+        grid.ipr(), 0 );
+
+    check(tri, diag);
+    reset();
+  }
+
+
+/*
+  // Container Interface
+  
+  if( grid.ipc() == 0) 
+    for( int i = 1; i < grid.npc(); ++i )
+      blacspp::trsd2d( grid, 4, 4, data_send, 4, grid.ipr(), i );
+  else
+    blacspp::trrv2d(grid, 4, 4, data_recv, 4, grid.ipr(), 0 );
+
+  check();
+*/
+
+
+
+}
+
+TEST( SEND_RECV, full_blacs_int ) {
+  ge_send_recv_test< blacspp::blacs_int >();
+  tr_send_recv_test< blacspp::blacs_int >();
+}
+TEST( SEND_RECV, full_float ) {
+  ge_send_recv_test< float >();
+}
+TEST( SEND_RECV, full_double ) {
+  ge_send_recv_test< double >();
+}
+TEST( SEND_RECV, full_scomplex ) {
+  ge_send_recv_test< blacspp::scomplex >();
+}
+TEST( SEND_RECV, full_dcomplex ) {
+  ge_send_recv_test< blacspp::dcomplex >();
 }
