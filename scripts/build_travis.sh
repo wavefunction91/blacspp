a44ccbaffaccbbbc4d95bf01c4e6f44a269396e4
David Williams-Young <dbwy@lbl.gov>
2020-02-20 12:42:11 -0500

    Find ScaLAPACK...

--- scripts/build_travis.sh
+++ scripts/build_travis.sh
@@ -4,8 +4,8 @@ echo $PWD
 mpicc  -v
 
 ls /usr/lib
-ls /usr/lib64
 ls /usr/local/lib
-ls /usr/local/lib64
+
+dpkg -L libscalapack
 
 /usr/bin/cmake -H. -B$BUILD_PREFIX -DCMAKE_TOOLCHAIN_FILE=$PWD/scripts/travis-toolchain.cmake -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX
