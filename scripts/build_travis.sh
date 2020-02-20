724983619b46b63645cbd8674699e69c8549b6e7
David Williams-Young <dbwy@lbl.gov>
2020-02-20 12:35:01 -0500

    Finding where Travis installs ScaLAPACK

--- scripts/build_travis.sh
+++ scripts/build_travis.sh
@@ -2,6 +2,10 @@
 
 echo $PWD
 mpicc  -v
-mpic++ -v
+
+ls /usr/lib
+ls /usr/lib64
+ls /usr/local/lib
+ls /usr/local/lib64
 
 /usr/bin/cmake -H. -B$BUILD_PREFIX -DCMAKE_TOOLCHAIN_FILE=$PWD/scripts/travis-toolchain.cmake -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX
