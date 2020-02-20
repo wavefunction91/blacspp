0fa739b0f01d90cdf1f57204d187b81e8df5797b
David Williams-Young <dbwy@lbl.gov>
2020-02-20 12:16:37 -0500

    Output MPI info

--- scripts/build_travis.sh
+++ scripts/build_travis.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
 echo $PWD
+which mpicc
+which mpic++
 
 /usr/bin/cmake -H. -B$BUILD_PREFIX -DCMAKE_TOOLCHAIN_FILE=$PWD/scripts/travis-toolchain.cmake -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX
