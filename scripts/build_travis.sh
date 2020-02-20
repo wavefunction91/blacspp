8e3b443f141eed23f507ff75efe53466f35d53fb
David Williams-Young <dbwy@lbl.gov>
2020-02-20 13:22:11 -0500

    Added build and test

--- scripts/build_travis.sh
+++ scripts/build_travis.sh
@@ -3,9 +3,8 @@
 echo $PWD
 mpicc  -v
 
-ls /usr/lib
-ls /usr/local/lib
-
 dpkg -L libscalapack-mpich-dev 
 
 /usr/bin/cmake -H. -B$BUILD_PREFIX -DCMAKE_TOOLCHAIN_FILE=$PWD/scripts/travis-toolchain.cmake -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX
+make -j4 -C $BUILD_PREFIX install
+make test
