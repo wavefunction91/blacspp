a4d73136061a677ca911206a20ab2a491efe8fa1
David Williams-Young <dbwy@lbl.gov>
2020-02-20 12:50:45 -0500

    Update ScaLAPACK location in toolchain

--- scripts/travis-toolchain.cmake
+++ scripts/travis-toolchain.cmake
@@ -2,4 +2,4 @@ set(CMAKE_C_COMPILER       gcc-$ENV{GCC_VERSION}      )
 set(CMAKE_CXX_COMPILER     g++-$ENV{GCC_VERSION}      ) 
 set(CMAKE_Fortran_COMPILER gfortran-$ENV{GCC_VERSION} )
 
-set( blacs_LIBRARIES "-lscalapack -llapack -lblas" )
+set( blacs_LIBRARIES "/usr/lib/x86_64-linux-gnu/libscalapack-mpich.so" )
