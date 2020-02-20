20f79acff1d2af0a899f54e1b764773b7c7b98c9
David Williams-Young <dbwy@lbl.gov>
2020-02-20 12:23:46 -0500

    Getting more MPI info + adding blacs libs

--- scripts/build_travis.sh
+++ scripts/build_travis.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo $PWD
-which mpicc
-which mpic++
+mpicc  --version
+mpic++ --version
 
 /usr/bin/cmake -H. -B$BUILD_PREFIX -DCMAKE_TOOLCHAIN_FILE=$PWD/scripts/travis-toolchain.cmake -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX
