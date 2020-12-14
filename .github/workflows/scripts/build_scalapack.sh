a878d62ce877f455f5cbfffab111c108968cafdf
David Williams-Young <dbwy@lbl.gov>
2020-12-14 11:55:08 -0800

    [CI] Added ScaLAPACK to the build

---
+++ .github/workflows/scripts/build_scalapack.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# Exit on error
+set -ev
+export CC=/usr/bin/gcc
+export CXX=/usr/bin/g++
+export FC=/usr/bin/gfortran
+
+# Print compiler information
+$CC --version
+$CXX --version
+$FC --version
+
+# log the CMake version (need 3+)
+cmake --version
+
+git clone https://github.com/Reference-ScaLAPACK/scalapack.git
+cd scalapack
+
+export INSTALL_DIR=$1
+export BUILD_DIR=$2
+cmake -H. -B$BUILD_DIR -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR -DCMAKE_C_COMPILER=$CC \
+      -DCMAKE_CXX_COMPILER=$CXX -DCMAKE_Fortran_COMPILER=$FC
+make -C $BUILD_DIR -j2 install
